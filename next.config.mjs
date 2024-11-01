e783df009f172ba8922b9586a5532386a24bbfad
Explorimentalist <73285549+Explorimentalist@users.noreply.github.com>
2024-11-01 21:54:37 +0000

    commit changes

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.NODE_ENV === 'production' ? '/calendar-app' : '',
+  distDir: 'out',
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
@@ -17,7 +18,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './app'),
     }
     return config
   },

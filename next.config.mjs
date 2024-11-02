d7488c4f00df54b4a28596fb0bbfac960ba9c908
Explorimentalist <73285549+Explorimentalist@users.noreply.github.com>
2024-11-02 16:02:58 +0000

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,6 @@ const nextConfig = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, './app'),
-      '@/lib': path.resolve(__dirname, './lib'),
     }
     return config
   },
